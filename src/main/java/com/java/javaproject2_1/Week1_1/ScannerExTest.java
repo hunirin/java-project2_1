3307e8db7b4006bb07c6a179eb74b1848debed78
hun053@naver.com <hun053@naver.com>
2023-04-19 15:28:01 +0900

    feat: ScannerEx, ScannerExTest 추가 - 숫자를 받아 더하는 코드

---
+++ src/main/java/com/java/javaproject2_1/Week1_1/ScannerExTest.java
@@ -0,0 +1,8 @@
+package com.java.javaproject2_1.Week1_1;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumbersAndPlus();
+    }
+}
