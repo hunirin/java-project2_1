e900cec6209b62c7aa6434ed7f9c1533f03b92c1
hun053@naver.com <hun053@naver.com>
2023-04-19 14:20:40 +0900

    feat: JavaInput JavaInputTest

---
+++ src/main/java/com/java/javaproject2_1/week1/JavaInputTest.java
@@ -0,0 +1,10 @@
+package com.java.javaproject2_1.week1;
+
+import java.io.IOException;
+
+public class JavaInputTest {
+    public static void main(String[] args) throws IOException {
+        JavaInput JavaInput = new JavaInput();
+        JavaInput.readTwoChars();
+    }
+}
