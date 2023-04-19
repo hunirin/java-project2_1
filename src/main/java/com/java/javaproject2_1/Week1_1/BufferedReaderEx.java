f407bf3c913cc447a37f2f2b97471386a6f7d8b6
hun053@naver.com <hun053@naver.com>
2023-04-19 15:27:25 +0900

    feat: BufferedReaderEx, BufferedReaderExTest 추가 - 내용을 입력받아 저장하는 코드

--- src/main/java/com/java/javaproject2_1/Week1_1/BufferedReaderEx.java
+++ src/main/java/com/java/javaproject2_1/Week1_1/BufferedReaderEx.java
@@ -9,10 +9,14 @@ public class BufferedReaderEx {
         InputStreamReader is = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(is); // 카트 가지고 오기
 
-        String line1 = br.readLine();
-        System.out.println("line1 =" + line1);
-        String line2 = br.readLine();
-        System.out.println("line2 =" + line2);
+        System.out.println("line1 =" + br.readLine());
+        System.out.println("line2 =" + br.readLine());
+
+    }
+    public void plusTwoNumbers() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is); // 카트 가지고 오기
+        System.out.println(br.read() + br.read());
     }
 
 }
