fe789255fcf42aebf6c76e4353a058c15b53bcf2
hun053@naver.com <hun053@naver.com>
2023-04-19 13:30:13 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/com/java/javaproject2_1/CrawlDate.java
@@ -0,0 +1,12 @@
+package com.java.javaproject2_1;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+    // alt + insert
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+}
