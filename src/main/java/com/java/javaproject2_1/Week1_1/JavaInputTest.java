70a07ce2b4bbbc566c64ed74a5abf0cc4dcec425
hun053@naver.com <hun053@naver.com>
2023-04-19 14:30:14 +0900

    feat: JavaInput JavaInputTest 추가

---
+++ src/main/java/com/java/javaproject2_1/Week1_1/JavaInputTest.java
@@ -0,0 +1,10 @@
+package com.java.javaproject2_1.Week1_1;
+
+import java.io.IOException;
+
+public class JavaInputTest {
+    public static void main(String[] args) throws IOException {
+        JavaInput JavaInput = new JavaInput();
+        JavaInput.readTwoChars();
+    }
+}
