3595764cbf94be75c2544a0c2e4fb34c9d2e8c92
hun053@naver.com <hun053@naver.com>
2023-04-20 11:41:26 +0900

    feat: codeup 1010 - printf 사용

--- src/main/java/com/java/javaproject2_1/codeup/_1010.java
+++ src/main/java/com/java/javaproject2_1/codeup/_1010.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class _1010 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt());
+        System.out.printf("%d", sc.nextInt());
+        // System.out.println(sc.nextInt());
+
 
     }
+
 }
