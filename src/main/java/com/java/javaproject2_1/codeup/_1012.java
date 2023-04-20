79022d1c35dbc2bc8c7eded9d9db5985992204cc
hun053@naver.com <hun053@naver.com>
2023-04-20 14:03:47 +0900

    feat: codeup 1011~1013

---
+++ src/main/java/com/java/javaproject2_1/codeup/_1012.java
@@ -0,0 +1,10 @@
+package com.java.javaproject2_1.codeup;
+
+import java.util.Scanner;
+
+public class _1012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%f", sc.nextFloat());
+    }
+}
