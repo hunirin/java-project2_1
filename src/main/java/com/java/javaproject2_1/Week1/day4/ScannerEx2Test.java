59f43572478ea6368d5409d5b1fb1586ba9d610d
hun053@naver.com <hun053@naver.com>
2023-04-20 10:43:51 +0900

    feat: ScannerEx2, ScannerEx2Test 추가 - 문자를 받는 안내문 추가

---
+++ src/main/java/com/java/javaproject2_1/Week1/day4/ScannerEx2Test.java
@@ -0,0 +1,8 @@
+package com.java.javaproject2_1.Week1.day4;
+
+public class ScannerEx2Test {
+    public static void main(String[] args) {
+        ScannerEx2 se = new ScannerEx2();
+        se.inputTwoNumbers();
+    }
+}
