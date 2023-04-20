df59ad1440839bc7fafaf4efd9db25b5787e5430
hun053@naver.com <hun053@naver.com>
2023-04-20 11:48:44 +0900

    feat: PrintfDate 추가 - 날짜 코드 출력 %02d 사용

---
+++ src/main/java/com/java/javaproject2_1/Week1/day4/PrintfDate.java
@@ -0,0 +1,8 @@
+package com.java.javaproject2_1.Week1.day4;
+
+public class PrintfDate {
+    public static void main(String[] args) {
+        System.out.printf("%d-%02d-%02d", 2023, 4, 20);
+
+    }
+}
