62175cd5abf037b321ce4e7077d4282ff774aa16
hun053@naver.com <hun053@naver.com>
2023-04-20 09:23:47 +0900

    feat: Scanner 복습 - ScannerEx, ScannerExTest 추가

---
+++ src/main/java/com/java/javaproject2_1/Week1_2/ScannerEx.java
@@ -0,0 +1,11 @@
+package com.java.javaproject2_1.Week1_2;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public void readTwoNumberAndPlus() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
---
+++ src/main/java/com/java/javaproject2_1/Week1_2/ScannerExTest.java
@@ -0,0 +1,8 @@
+package com.java.javaproject2_1.Week1_2;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumberAndPlus();
+    }
+}
