cfb30b7101466601332f37f18e0de2e3ad856f44
hun053@naver.com <hun053@naver.com>
2023-04-20 10:01:10 +0900

    feat: codeup 1006~1008 풀이

---
+++ src/main/java/com/java/javaproject2_1/codeup/_1007.java
@@ -0,0 +1,7 @@
+package com.java.javaproject2_1.codeup;
+
+public class _1007 {
+    public static void main(String[] args) {
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+    }
+}
---
+++ src/main/java/com/java/javaproject2_1/codeup/_1008.java
@@ -0,0 +1,9 @@
+package com.java.javaproject2_1.codeup;
+
+public class _1008 {
+    public static void main(String[] args) {
+        System.out.println("\u250c\u252c\u2510");
+        System.out.println("\u251C\u253C\u2524");
+        System.out.println("\u2514\u2534\u2518");
+    }
+}
