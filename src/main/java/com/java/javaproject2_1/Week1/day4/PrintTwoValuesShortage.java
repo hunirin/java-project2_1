9c6377512f4addfb96ca7b94527b852bf63e9628
hun053@naver.com <hun053@naver.com>
2023-04-20 11:29:12 +0900

    feat: printf 실습

---
+++ src/main/java/com/java/javaproject2_1/Week1/day4/PrintTwoValuesShortage.java
@@ -0,0 +1,8 @@
+package com.java.javaproject2_1.Week1.day4;
+
+public class PrintTwoValuesShortage {
+    public static void main(String[] args) {
+        System.out.printf("%s님 안녕하세요. 그런데 제가 하고 싶은 말은 %s입니다.",
+                "이광훈","똥마려");
+    }
+}
