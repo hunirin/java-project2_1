1b34e35e76fdfaef73dff9eb4609587516c09791
hun053@naver.com <hun053@naver.com>
2023-04-20 14:35:52 +0900

    feat: byte에 저장 예시

---
+++ src/main/java/com/java/javaproject2_1/Week1/day4/ByteTypeExercise.java
@@ -0,0 +1,14 @@
+package com.java.javaproject2_1.Week1.day4;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = -128;
+        byte byte3 = 127;
+
+        System.out.println(byte1);
+        System.out.println(byte2);
+        System.out.println(byte3);
+
+    }
+}
