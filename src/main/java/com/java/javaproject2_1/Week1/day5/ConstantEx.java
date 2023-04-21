08eea60ac20833afa0f3ac4bfe2649b9acad6731
hun053@naver.com <hun053@naver.com>
2023-04-21 11:23:00 +0900

    feat: ConstantEx 추가 - 상수 실습

---
+++ src/main/java/com/java/javaproject2_1/Week1/day5/ConstantEx.java
@@ -0,0 +1,9 @@
+package com.java.javaproject2_1.Week1.day5;
+
+public class ConstantEx {
+    public static void main(String[] args) {
+        final int iVal; // int 앞에 final을 붙임
+        iVal = 1; // 최초의 한 번만 값을 지정 가능
+        System.out.println(iVal);
+    }
+}
