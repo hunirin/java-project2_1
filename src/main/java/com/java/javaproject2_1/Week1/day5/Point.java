be831c7a49448c7e19e795d1b6307d79343fb996
hun053@naver.com <hun053@naver.com>
2023-04-21 14:49:56 +0900

    feat: Point, PointTest 추가 - 점 위치 표현 - 불리언 사용

---
+++ src/main/java/com/java/javaproject2_1/Week1/day5/Point.java
@@ -0,0 +1,13 @@
+package com.java.javaproject2_1.Week1.day5;
+
+public class Point {
+    public int x;
+    public int y;
+
+    boolean isSameXy() {
+        // x와 y가 같니?
+        return x == y;
+    }
+
+
+}
