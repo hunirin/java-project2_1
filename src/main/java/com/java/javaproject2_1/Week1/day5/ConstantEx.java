6a73f5723e4a6cb849c83d346aad6dbc4493ad9d
hun053@naver.com <hun053@naver.com>
2023-04-21 11:44:12 +0900

    feat: ConstantEx 추가 - 상수 실습

--- src/main/java/com/java/javaproject2_1/Week1/day5/ConstantEx.java
+++ src/main/java/com/java/javaproject2_1/Week1/day5/ConstantEx.java
@@ -1,5 +1,7 @@
 package com.java.javaproject2_1.Week1.day5;
 
+import com.java.javaproject2_1.PrintHello;
+
 public class ConstantEx {
     public final int MAX_LEVEL = 3;
     public static final int MIN_LEVEL = 1;
@@ -8,5 +10,8 @@ public class ConstantEx {
         final int iVal; // int 앞에 final을 붙임
         iVal = 1; // 최초의 한 번만 값을 지정 가능
         System.out.println(iVal);
+
+        final PrintHello printHello = new PrintHello();
+        
     }
 }
