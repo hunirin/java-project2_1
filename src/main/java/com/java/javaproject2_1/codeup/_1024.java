d6a037d7f58d522a723e57cd5acb6732ec81ca92
hun053@naver.com <hun053@naver.com>
2023-04-25 17:10:32 +0900

    feat: codeup 1024~25 풀이

---
+++ src/main/java/com/java/javaproject2_1/codeup/_1024.java
@@ -0,0 +1,15 @@
+package com.java.javaproject2_1.codeup;
+
+import java.util.Scanner;
+
+public class _1024 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] str = sc.nextLine().toCharArray();
+
+        for (int i = 0; i < str.length; i++) {
+            System.out.printf("\'%c\'\n", str[i]);
+        }
+    }
+}
