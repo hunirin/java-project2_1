719d8711fe61bab0d67e9142ba48ad41a850a469
hun053@naver.com <hun053@naver.com>
2023-04-25 15:42:03 +0900

    feat: codeup 1038~40 풀이

---
+++ src/main/java/com/java/javaproject2_1/codeup/_1038_2.java
@@ -0,0 +1,14 @@
+package com.java.javaproject2_1.codeup;
+
+import java.util.Scanner;
+
+public class _1038_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long val1 = sc.nextLong();
+        long val2 = sc.nextLong();
+
+        System.out.printf("%d", val1+val2);
+
+    }
+}
---
+++ src/main/java/com/java/javaproject2_1/codeup/_1040.java
@@ -0,0 +1,13 @@
+package com.java.javaproject2_1.codeup;
+
+import java.util.Scanner;
+
+public class _1040 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int iVal = sc.nextInt();
+
+        System.out.printf("%d", -iVal);
+    }
+}
