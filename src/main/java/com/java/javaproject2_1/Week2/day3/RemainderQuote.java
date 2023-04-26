191d67428c982d1d159338f64eb55f92c44587f4
hun053@naver.com <hun053@naver.com>
2023-04-26 14:09:02 +0900

    feat: 연산자 실습 - 몫 나머지 구하기

---
+++ src/main/java/com/java/javaproject2_1/Week2/day3/RemainderQuote.java
@@ -0,0 +1,14 @@
+package com.java.javaproject2_1.Week2.day3;
+
+public class RemainderQuote {
+    public static void main(String[] args) {
+        int iVal = 1;
+        System.out.println(iVal/2);
+
+        // 몫
+        int quote = 10 / 3;
+        // 나머지
+        int remainder = 10 % 3;
+        System.out.printf("10 / 3의 몫은 %d이고, 나머지는 %d이다", quote, remainder);
+    }
+}
