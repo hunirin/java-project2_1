cdd43b481ecbfbd4b9823e4b47bbd908ae7e9c57
hun053@naver.com <hun053@naver.com>
2023-04-26 15:43:54 +0900

    feat: 숫자, 문자열 누적하기

---
+++ src/main/java/com/java/javaproject2_1/Week2/day3/LikeAccumulate.java
@@ -0,0 +1,17 @@
+package com.java.javaproject2_1.Week2.day3;
+
+public class LikeAccumulate {
+    public static void main(String[] args) {
+        int likeCount = 0;
+        likeCount = likeCount +1;
+        System.out.println(likeCount); // 좋아요 한 개
+
+        likeCount = likeCount +1;
+        System.out.println(likeCount); // 좋아요 두 개
+
+        likeCount = likeCount -1;
+        System.out.println(likeCount); // 좋아요 한 개
+
+
+    }
+}
---
+++ src/main/java/com/java/javaproject2_1/Week2/day3/StringAccumulate.java
@@ -0,0 +1,12 @@
+package com.java.javaproject2_1.Week2.day3;
+
+public class StringAccumulate {
+    public static void main(String[] args) {
+        String answer = "";
+        answer = answer + "쿵";
+        System.out.println(answer); // '쿵'이 출력
+        answer = answer + "짝";
+        System.out.println(answer); // '쿵짝'이 출력
+
+    }
+}
