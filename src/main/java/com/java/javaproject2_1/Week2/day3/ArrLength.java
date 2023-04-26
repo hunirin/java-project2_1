0bf96fc6075e14356834ae25a302100338fd90ce
hun053@naver.com <hun053@naver.com>
2023-04-26 13:38:14 +0900

    feat: 배열 길이 예시

---
+++ src/main/java/com/java/javaproject2_1/Week2/day3/ArrLength.java
@@ -0,0 +1,9 @@
+package com.java.javaproject2_1.Week2.day3;
+
+public class ArrLength {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        //배열의 길이 표현
+        System.out.println(arr.length);
+    }
+}
