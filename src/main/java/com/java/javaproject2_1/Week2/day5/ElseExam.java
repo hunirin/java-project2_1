d7ee48465c4e48a8f0e2938a0a869ec8688dd366
hun053@naver.com <hun053@naver.com>
2023-04-28 09:45:26 +0900

    feat: if, else 실습

---
+++ src/main/java/com/java/javaproject2_1/Week2/day5/ElseExam.java
@@ -0,0 +1,19 @@
+package com.java.javaproject2_1.Week2.day5;
+
+public class ElseExam {
+    public static void main(String[] args) {
+        boolean checkTrue = true;
+        if (checkTrue) {
+            System.out.println("true입니다.");
+        } else {
+            System.out.println("false입니다.");
+        }
+
+        boolean checkFalse = false;
+        if (checkTrue) {
+            System.out.println("true입니다.");
+        } else {
+            System.out.println("false입니다.");
+        }
+    }
+}
---
+++ src/main/java/com/java/javaproject2_1/Week2/day5/IfEx.java
@@ -0,0 +1,9 @@
+package com.java.javaproject2_1.Week2.day5;
+
+public class IfEx {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("항상 실행 됩니다.");
+        }
+    }
+}
