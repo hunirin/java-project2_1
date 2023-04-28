01d2e67f0dc4f900c80f6e650825cb8ff4f72406
hun053@naver.com <hun053@naver.com>
2023-04-28 09:45:38 +0900

    feat: codeup 1065~67

---
+++ src/main/java/com/java/javaproject2_1/codeup/_1067.java
@@ -0,0 +1,28 @@
+package com.java.javaproject2_1.codeup;
+
+import java.util.Scanner;
+
+public class _1067 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if(a % 2 == 0) {
+            if(a >= 0) {
+                System.out.println("plus");
+            } else {
+                System.out.println("minus");
+            }
+            System.out.println("even");
+
+        } else {
+            if(a >= 0) {
+                System.out.println("plus");
+            } else {
+                System.out.println("minus");
+            }
+            System.out.println("odd");
+        }
+    }
+}
