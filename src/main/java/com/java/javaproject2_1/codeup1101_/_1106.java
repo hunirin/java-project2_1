cfc805958cb53462e901db203e0570add268a35c
hun053@naver.com <hun053@naver.com>
2023-05-01 09:18:25 +0900

    feat: codeup 1103,06

---
+++ src/main/java/com/java/javaproject2_1/codeup1101_/_1106.java
@@ -0,0 +1,7 @@
+package com.java.javaproject2_1.codeup1101_;
+
+public class _1106 {
+    public static void main(String[] args) {
+        System.out.printf("%d %d", Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+}
