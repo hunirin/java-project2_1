68b1f3dcb8bb91aba601ac9b4824c7f21c7960eb
hun053@naver.com <hun053@naver.com>
2023-05-01 13:07:53 +0900

    feat: 반복문 역순 실습

---
+++ src/main/java/com/java/javaproject2_1/Week3/day1/ForLoopReverse.java
@@ -0,0 +1,10 @@
+package com.java.javaproject2_1.Week3.day1;
+
+public class ForLoopReverse {
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+            // System.out.println(i);
+            System.out.printf("%d ", i);
+        }
+    }
+}
