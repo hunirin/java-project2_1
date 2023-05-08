2702babbf459a953869ed005db8124d1ac85d6c8
hun053@naver.com <hun053@naver.com>
2023-05-08 10:02:00 +0900

    feat: 코드업 1367 - 평행 사변형 만들기

---
+++ src/main/java/com/java/javaproject2_1/codeup1200_/_1367.java
@@ -0,0 +1,22 @@
+package com.java.javaproject2_1.codeup1200_;
+
+import java.util.Scanner;
+
+public class _1367 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i > 0; i-- ) {
+
+            for (int j = 0; j < i-1; j++) {
+                System.out.printf(" ");
+            }
+            for (int j = 0; j < n; j++) {
+                System.out.printf("*");
+            }
+            System.out.println("");
+        }
+    }
+}
